0e0305e87e670b7912eaaaa50929ed65f3f3f73b
lucianokim <luciano.kim47@gmail.com>
2025-05-16 16:52:02 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
-
-from flask import Flask, jsonify
+from flask import Flask, Response
 import requests
+import json
 
 app = Flask(__name__)
 
 @app.route("/api")
-def call_api():
+def get_data():
     url = "https://api.odcloud.kr/api/15077756/v1/vaccine-stat"
     params = {
         "serviceKey": "여기에_당신의_API_KEY_입력",
         "page": 1,
         "perPage": 10
     }
-    response = requests.get(url, params=params)
-    return jsonify(response.json())
+    res = requests.get(url, params=params)
+    return Response(
+        json.dumps(res.json(), ensure_ascii=False),
+        content_type="application/json; charset=utf-8"
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
